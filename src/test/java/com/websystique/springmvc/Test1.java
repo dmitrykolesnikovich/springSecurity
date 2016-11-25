6cd7c689b9ff0c5bf05c9a50f1772f6816803c75
dkolesnikovich <dkolesnikovich@intervale.ru>
2016-11-25 17:30:14 +0300

    add test

---
+++ src/test/java/com/websystique/springmvc/Test1.java
@@ -0,0 +1,16 @@
+package com.websystique.springmvc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test1 {
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+        assertEquals("test1", "test1");
+    }
+
+}
